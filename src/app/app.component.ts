ee838377c18505bdcad6d63993789a46f2e89dba
A1ien1385 <damian.zawadzki@tlen.pl>
2024-12-21 17:43:13 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PostsComponent } from './posts/posts.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PostsComponent],
+  imports: [PostsComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
